ddf4e8e6c3f491ef6c562865feeaa285991566bb
saharsh <srpatel56@myseneca.ca>
2024-08-13 15:01:56 -0400

    fixed the build errors

--- src/_theme.ts
+++ src/_theme.ts
@@ -81,4 +81,4 @@ const theme = extendTheme({
   },
 });
 
-export default theme;
+export { theme };
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,6 +1,6 @@
 import { Html, Head, Main, NextScript } from 'next/document';
 import { ColorModeScript } from '@chakra-ui/react';
-import theme from './_theme'; // Make sure your path is correct
+import { theme } from '../_theme'; // Make sure your path is correct
 
 export default function Document() {
   return (
