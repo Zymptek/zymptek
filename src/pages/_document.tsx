d1753e286500898229cccb6225d9a764d6a0823c
saharsh <srpatel56@myseneca.ca>
2024-08-16 10:51:32 -0400

    updated country code

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -35,7 +35,7 @@ export default function Document() {
           "addressLocality": "ontario, Canada",
           "postalCode": "M3J1V6",
           "streetAddress": "470 Sentinel Road",
-          "addressCountry": "Canada"
+          "addressCountry": "CA"
         },
         "email": "zymptek@gmail.com",
         "name": "Zymptek"
